ebc920c719408597fddf03a8f8c1fef030c6f841
PrashantSharma6 <prashantsharma6457@gmail.com>
2024-12-10 12:17:02 +0530

    Add project files.

---
+++ ReadNDeals/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReadNDeals.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
